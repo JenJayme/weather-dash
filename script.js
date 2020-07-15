f4f692d7a78781803693f2aa247671aa0274c467
Jen Jayme <jeniferjayme@gmail.com>
2020-07-14 18:49:22 -0700

    Added working 5-day forecast API & today icon append function.

--- script.js
+++ script.js
@@ -11,7 +11,8 @@
 // THEN I am again presented with current and future conditions for that city
 
 var searchHistory = [];
-
+var fiveDayData = [];
+var weatherToday = {};
 //API requests
 //sample forecast 16-day https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
 
@@ -31,70 +32,90 @@ const paramsForecast = "forecast/daily" + "?key=" + API_KEY + "&units=i&city=" +
 var city = "Novato";
 var state = "CA";
 var cityState = city + "," + state;
-const testURL = "http://api.weatherbit.io/v2.0/current?key=314f8e60676745089873728b30174cc3&units=i&city=Novato,CA";
-const icons = "https://www.weatherbit.io/static/img/icons/"
+var today = (moment().format('MMMM DD, YYYY'));
 
-var forecastQueryURL = "http://api.weatherbit.io/v2.0/forecast/daily" + "?key=" + API_KEY + "&units=i&city=" + cityState + "&days=5";
+const testURL = "http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5";
+const weatherNowQueryURL = "http://api.weatherbit.io/v2.0/current" + "?key=" + API_KEY + "&city=" + cityState;
+const forecastQueryURL = "http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5";
+const testIcon = "https://www.weatherbit.io/static/img/icons/c01d.png";
+const iconHub = "https://www.weatherbit.io/static/img/icons/";
 
-var weatherNowQueryURL = "http://api.weatherbit.io/v2.0/current" + "?key=" + API_KEY + "&city=" + cityState;
+var todayWeatherObj = {};  
 
-function weatherNow () {
+function weatherNow() {
     $.ajax({
         url: weatherNowQueryURL,
         method: "GET"
     }).then(function (response) {
-        var location = $("#city-state").text(cityState);
-        var date = $("#date").text(response.data[0].ob_time);
-        var icon = $("#icon").text(response.data[0].weather.icon);
-        var currentTemp = $("#current-temp").text(response.data[0].app_temp);
-        var currentHumidity = $("#current-humidity").text(response.data[0].rh);
-        var currentWindSpeed = $("#current-wind-speed").text(response.data[0].wind_spd);
-        var currentWindDirection = $("#current-wind-direction").text(response.data[0].wind_cdir_full);
-        var currentUV = $("#current-uv").text(response.data[0].uv);
-        console.log(response);
+        todayWeatherObj.location = $("#city-state").text(cityState);
+        todayWeatherObj.date = $("#date").text(today);
+        todayWeatherObj.currentTemp = $("#current-temp").text(response.data[0].app_temp);
+        todayWeatherObj.currentHumidity = $("#current-humidity").text(response.data[0].rh);
+        todayWeatherObj.currentWindSpeed = $("#current-wind-speed").text(response.data[0].wind_spd);
+        todayWeatherObj.currentWindDirection = $("#current-wind-direction").text(response.data[0].wind_cdir_full);
+        todayWeatherObj.currentUV = $("#current-uv").text(response.data[0].uv);
+        // todayWeatherObj.iconCode = $("#icon").text(response.data[0].weather.icon);
+        todayWeatherObj.iconCode = response.data[0].weather.icon;
+        todayWeatherObj.description = $("#today-description").text(response.data[0].weather.description);
     });
 };
 
-function weatherForecast () {
+// var icon = appendIcon(response.data[0].weather.icon);
+
+function appendIcon(iconCode) {
+    var iconSrc = (iconHub + iconCode + ".png");
+    $("#icon").append('<img id="icon" src=iconSrc />');
+    console.log(iconHub);
+    console.log(iconSrc);
+}
+        // var icon = $("#icon").append('<img src="https://www.weatherbit.io/static/img/icons/c01d.png' />);
+
+function weatherForecast() {
     $.ajax({
-        url: weatherNowQueryURL,
+        url: forecastQueryURL,
         method: "GET"
     }).then(function (response) {
-        var location = $("#city-state").text(cityState);
-        var date = $("#date").text(response.data[0].ob_time);
-        var icon = $("#icon").text(response.data[0].weather.icon);
-        var currentTemp = $("#current-temp").text(response.data[0].app_temp);
-        var currentHumidity = $("#current-humidity").text(response.data[0].rh);
-        var currentWindSpeed = $("#current-wind-speed").text(response.data[0].wind_spd);
-        var currentWindDirection = $("#current-wind-direction").text(response.data[0].wind_cdir_full);
-        var currentUV = $("#current-uv").text(response.data[0].uv);
-        console.log(response);
-    });
-};
+        var div, date, dateCol, dl, dd;
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data[i]);
 
-//fields from current
-// data.
-//     "app_temp" current temperature 
-// min_temp
-// max_temp
-// city_name
-// state_code
-// Weather.description
-//ob_time date -  use moment.js for date format conversion
-//weather.icon - to show icon, append weather.icon + .png, to "https://www.weatherbit.io/static/img/icons/" e.g., 
-//https://www.weatherbit.io/static/img/icons/r01d.png
-// wind_spd
-// wind_cdir_full
-// uv
-// rh
+            // console.log(response.data[i].datetime)
+            date = (moment(response.data[i].datetime).format('MMMM DD'));        
+
+            div = $("#forecast-div").append("<div>");
+            // div = $('<div></div>');
+            div.addClass("col-2");
+            div.append("<dl>");
+
+            div = $("#forecast-div").append("<div>").addClass("col-2");
+            dateCol = $("dl").append("<dt>"+ date +"</dt>")
+            // date = $("#date-my").text(moment().format('MM DD')
+            
+            $("dl").append("<dd>"+response.data[i].rh+"</dd>");
+            $("dl").append("<dd>"+response.data[i].weather.description+"</dd>");
+            
+            // $("dd").append("<dd>").text(response.data[i].rh)
+            $("#fore-date").text(response.data[i].datetime);
+            $("#fore-icon").text(response.data[i].weather.icon);
+            // dl = $("dl").append("<dd>list item</dd>");
+            // .text(response.data[i].datetime);
+        }
+    });
+}
+
+        // Create Question div
+        // div = $('<div></div>');
+        // div.addClass('question');
+        // div.text(QNAObject.question);
+        // questionsContainer.append(div);
 
 //==============================================================
 
 //push searched cities to searchHistory
 function pushCitiesToArray() {
-    searchHistory.push(food1);
-    searchHistory.push(food2);
-    searchHistory.push(food3);
+    searchHistory.push(city1);
+    searchHistory.push(city2);
+    searchHistory.push(city3);
 };
 
 //saves the search history to local storage
@@ -136,6 +157,7 @@ function loadFromLocalStorage() {
 $(document).ready(function () {
 
     weatherNow();
+    weatherForecast(5);
     // postData();
 
 });
