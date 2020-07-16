65c8de3c067481a8b4aa1e3cdbcc2ba855e7b0e6
Jen Jayme <jeniferjayme@gmail.com>
2020-07-15 17:31:09 -0700

    Added uv color function, styling w new color palette, fixed today weather icon.

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 
 var searchHistory = [];
-var forecastData = [];
-var weatherToday = {};
+var todayWeatherObj = {};
+var forecastWeatherArr = [];
+
 //API requests
 //sample forecast 16-day https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
-
 //sample current http://api.weatherbit.io/v2.0/current?key=314f8e60676745089873728b30174cc3&units=i&city=Novato,CA
-
 //sample forecast 5-day http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5
-
-//endpt for daily "HTTP: http://api.weatherbit.io/v2.0/forecast/daily"
-//endput for curret "HTTP: http://api.weatherbit.io/v2.0/current"
 
 
 const API_KEY = "314f8e60676745089873728b30174cc3";
@@ -28,9 +24,8 @@ const weatherNowQueryURL = "http://api.weatherbit.io/v2.0/current" + "?key=" + A
 const forecastQueryURL = "http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5";
 const testIcon = "https://www.weatherbit.io/static/img/icons/c01d.png";
 const iconHub = "https://www.weatherbit.io/static/img/icons/";
-
-var todayWeatherObj = {};
-var forecastWeatherArr = [];
+var iconSrc;
+var iconCode;
 
 function weatherNow() {
     $.ajax({
@@ -39,33 +34,54 @@ function weatherNow() {
     }).then(function (response) {
         todayWeatherObj.location = $("#city-state").text(cityState);
         todayWeatherObj.date = $("#date").text(today);
-        todayWeatherObj.currentTemp = $("#current-temp").text(response.data[0].app_temp);
-        todayWeatherObj.currentHumidity = $("#current-humidity").text(response.data[0].rh);
-        todayWeatherObj.currentWindSpeed = $("#current-wind-speed").text(response.data[0].wind_spd);
+        todayWeatherObj.currentTemp = $("#current-temp").text(response.data[0].app_temp+'°F');
+        todayWeatherObj.currentHumidity = $("#current-humidity").text(response.data[0].rh+'%');
+        todayWeatherObj.currentWindSpeed = $("#current-wind-speed").text(response.data[0].wind_spd+'mph');
         todayWeatherObj.currentWindDirection = $("#current-wind-direction").text(response.data[0].wind_cdir_full);
         todayWeatherObj.currentUV = $("#current-uv").text(response.data[0].uv);
-        // todayWeatherObj.iconCode = $("#icon").text(response.data[0].weather.icon);
-        todayWeatherObj.iconCode = response.data[0].weather.icon;
+        todayWeatherObj.iconCode = $("#icon").append(`<img src=`+iconHub+response.data[0].weather.icon+`.png/>`);
         todayWeatherObj.description = $("#today-weather-description").text(response.data[0].weather.description);
+        iconCode = todayWeatherObj.iconCode;
     });
 };
 
-// var icon = appendIcon(response.data[0].weather.icon);
-
-function appendIcon(iconCode) {
-    var iconSrc = (iconHub + iconCode + ".png");
-    $("#icon").append('<img id="icon" src=iconSrc />');
-    console.log(iconHub);
-    console.log(iconSrc);
+function uvColor(uv) {
+    console.log("uv is "+uv)
+    if (uv < 3) {
+        $('#current-uv').addClass('uv-low') //green
+    } else if (uv > 2 && uv < 6) {
+        $('#current-uv').addClass('uv-mod-lo') //yellow
+    } else if (uv > 5 && uv < 8) {
+        $('#current-uv').addClass('uv-mod-hi') //orange   
+    } else if (uv >7 && uv < 10) {
+        $('#current-uv').addClass('uv-severe-lo') //red
+    } else {
+        $('#current-uv').addClass('uv-severe-hi') //red
+    }
 }
-// var icon = $("#icon").append('<img src="https://www.weatherbit.io/static/img/icons/c01d.png' />);
+//========================================================
+
+//UV INDEX INFO from EPA:
+//low 0-2 green
+// moderate 3-7 yellow or orange
+// severe 8+ red or purple
+
+//========================================================
+
+// Replaced with use of append & string literal in weathernow() 
+// function appendIcon(iconCode) {
+//     iconSrc = (iconHub + iconCode + ".png");
+//     $("#icon").append("<img src="+iconSrc+"/>");
+//     console.log(iconHub);
+//     console.log(iconSrc);
+// }
 
 function weatherForecast() {
     $.ajax({
         url: forecastQueryURL,
         method: "GET"
     }).then(function (response) {
-        var div, date, dateCol, dl, dd;
+        var date;
         let currentData;
         let numOfDays = response.data.length;
 
@@ -75,11 +91,11 @@ function weatherForecast() {
             iconSrc = (iconHub + currentData.weather.icon + ".png");
 
             $("#forecast-div").append(`<div class="col-2"><div class="card" style="width: 100%">
-            <div class="card-body"><img src=${iconSrc} class="card-img-top">
+            <div class="card-body"><img src=${iconSrc} class="card-img-top" />
             <h4 class="card-title text-center">${date}</h4>
-            <div class="text-center">${currentData.min_temp}</div>
-            <div class="text-center">${currentData.max_temp}</div>
-            <div class="text-center">${currentData.rh}</div>
+            <div class="text-center">Low: ${currentData.min_temp}</div>
+            <div class="text-center">High: ${currentData.max_temp}</div>
+            <div class="text-center">Humidity: ${currentData.rh}</div>
             </div></div></div>`);
         }
     });
@@ -104,13 +120,6 @@ function loadFromLocalStorage() {
 
 //========================================================
 
-//UV INDEX INFO from EPA:
-//low 0-2 green
-// moderate 3-7 yellow or orange
-// severe 8+ red or purple
-
-//========================================================
-
 // FORECAST DATA
 // data.city_name
 // state_code
@@ -132,6 +141,6 @@ $(document).ready(function () {
 
     weatherNow();
     weatherForecast(5);
-    // postData();
+    uvColor(9);
 
 });
