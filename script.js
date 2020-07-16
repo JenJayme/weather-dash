00f8f2efc7ce57bf0c37654390768e90e4509c02
Jen Jayme <jeniferjayme@gmail.com>
2020-07-15 21:11:31 -0700

    Added images and day to moment.js function.

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
+//========================================================
 
 var searchHistory = [];
 var todayWeatherObj = {};
 var forecastWeatherArr = [];
+var city = "Novato";
+var state = "CA";
+var cityState = city + "," + state;
+var today = (moment().format('dddd, MMMM Do, YYYY'));
+var d = moment().day()
+console.log("Day = "+d);
+
+//========================================================
+
+function getUserInputs() {
+    var locationObj;
+    searchCity = $('#city').val()
+    searchState = $('#startNum').val();
+
+    locationObj = {
+        searchCity: searchCity,
+        searchState: searchState,
+    };
+
+    searchHistory.push(locationObj);
+    return locationObj;
+}
+
+//push searched cities to searchHistory
+// function pushCitiesToArray(searchHistory) {
+//     searchHistory.push(city1);
+//     searchHistory.push(city2);
+//     searchHistory.push(city3);
+// };
+
+//saves the search history to local storage
+function saveToLocalStorage(item) {
+    localStorage.setItem(LS_KEY, JSON.stringify(item));
+};
+
+//reads the search history from local storage
+function loadFromLocalStorage(item) {
+    item = JSON.parse(localStorage.getItem(LS_KEY))
+};
+
+
+// console.log(locationObj);
+
+//========================================================
 
 //API requests
 //sample forecast 16-day https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
 //sample current http://api.weatherbit.io/v2.0/current?key=314f8e60676745089873728b30174cc3&units=i&city=Novato,CA
 //sample forecast 5-day http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5
 
-
 const API_KEY = "314f8e60676745089873728b30174cc3";
 const LS_KEY = "sunnyday";
 const wbEndPt = "http://api.weatherbit.io/v2.0/";
 const paramsCurrent = "current" + "?key=" + API_KEY + "&city=" + cityState;
 const paramsForecast = "forecast/daily" + "?key=" + API_KEY + "&units=i&city=" + cityState + "&days=5";
-var city = "Novato";
-var state = "CA";
-var cityState = city + "," + state;
-var today = (moment().format('MMMM DD, YYYY'));
 
 const testURL = "http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5";
 const weatherNowQueryURL = "http://api.weatherbit.io/v2.0/current" + "?key=" + API_KEY + "&city=" + cityState;
@@ -26,6 +66,8 @@ const testIcon = "https://www.weatherbit.io/static/img/icons/c01d.png";
 const iconHub = "https://www.weatherbit.io/static/img/icons/";
 var iconSrc;
 var iconCode;
+
+//========================================================
 
 function weatherNow() {
     $.ajax({
@@ -42,8 +84,11 @@ function weatherNow() {
         todayWeatherObj.iconCode = $("#icon").append(`<img src=`+iconHub+response.data[0].weather.icon+`.png/>`);
         todayWeatherObj.description = $("#today-weather-description").text(response.data[0].weather.description);
         iconCode = todayWeatherObj.iconCode;
+        uvColor(todayWeatherObj.currentUV);
     });
 };
+
+//========================================================
 
 function uvColor(uv) {
     console.log("uv is "+uv)
@@ -59,22 +104,8 @@ function uvColor(uv) {
         $('#current-uv').addClass('uv-severe-hi') //red
     }
 }
-//========================================================
-
-//UV INDEX INFO from EPA:
-//low 0-2 green
-// moderate 3-7 yellow or orange
-// severe 8+ red or purple
 
 //========================================================
-
-// Replaced with use of append & string literal in weathernow() 
-// function appendIcon(iconCode) {
-//     iconSrc = (iconHub + iconCode + ".png");
-//     $("#icon").append("<img src="+iconSrc+"/>");
-//     console.log(iconHub);
-//     console.log(iconSrc);
-// }
 
 function weatherForecast() {
     $.ajax({
@@ -88,6 +119,7 @@ function weatherForecast() {
         for (var i = 0; i < numOfDays; i++) {
             currentData = response.data[i];
             date = (moment(currentData.datetime).format('MMMM DD'));
+            // day = (moment().day('DD'));
             iconSrc = (iconHub + currentData.weather.icon + ".png");
 
             $("#forecast-div").append(`<div class="col-2"><div class="card" style="width: 100%">
@@ -101,46 +133,11 @@ function weatherForecast() {
     });
 }
 
-//push searched cities to searchHistory
-function pushCitiesToArray() {
-    searchHistory.push(city1);
-    searchHistory.push(city2);
-    searchHistory.push(city3);
-};
-
-//saves the search history to local storage
-function saveToLocalStorage() {
-    localStorage.setItem(LS_KEY, JSON.stringify(searchHistory));
-};
-
-//reads the search history from local storage
-function loadFromLocalStorage() {
-    searchHistory = JSON.parse(localStorage.getItem(LS_KEY))
-};
-
 //========================================================
-
-// FORECAST DATA
-// data.city_name
-// state_code
-// data.
-//     min_temp
-// max_temp
-// city_name
-// state_code
-// Weather.description
-//ob_time date, use moment.js for date format conversion
-//weather.icon to show icon, append weather.icon + .png, to "https://www.weatherbit.io/static/img/icons/" e.g.,
-//https://www.weatherbit.io/static/img/icons/r01d.png
-// wind_spd
-// wind_cdir_full
-// uv
-// rh
 
 $(document).ready(function () {
 
     weatherNow();
     weatherForecast(5);
-    uvColor(9);
 
 });
